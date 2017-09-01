9b0ee0bbf4a6cfb74b81a24569919d7af959418f
saliniag <salini.agarwal11@gmail.com>
2017-09-01 12:03:48 -0700

    implementing abstract method

---
+++ HRabstractClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRabstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+    abstract class Book
+    {
+        protected String title;
+        protected  String author;
+    
+        public Book(String t,String a){
+            title=t;
+            author=a;
+        }
+        public abstract void display();
+
+
+    }
+    class MyBook : Book
+    {
+        private int price;
+        //constructor
+       public MyBook(string t, string a, int p): base(t,a){
+            this.price = p;
+        }
+
+       public override void display()
+       {
+         Console.WriteLine("Title: "+this.title);
+         Console.WriteLine("Author: "+this.author);
+         Console.WriteLine("Price: " + this.price);
+       }
+
+    }
+}
